ad07b97b64c7973586b87dafc1d51f4e300863e5
tyler-wel <tylwelsh@gmail.com>
2020-03-07 14:16:21 +0900

    init commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
 import React from 'react';
-import logo from './logo.svg';
+import Countdown from 'react-countdown';
+import background from './assets/ac-back.jpg';
 import './App.css';
 
 function App() {
+  const releaseDate = new Date(2020, 2, 20, 0, 0, 0);
+  console.log(releaseDate)
+  const renderer = ({total, days, hours, minutes, seconds, complete }) => {
+    if (complete) {
+      return <span> Let's Play! </span>
+    } else {
+      return (
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-3">
+              <div>
+                {days}
+              </div>
+              <div>
+                日
+              </div>
+            </div>
+            <div className="col-sm-3">
+              <div>
+                {hours}
+              </div>
+              <div>
+                時
+              </div>
+            </div>
+            <div className="col-sm-3">
+              <div>
+                {minutes}
+              </div>
+              <div>
+                分
+              </div>
+            </div>
+            <div className="col-sm-3">
+              <div>
+                {seconds}
+              </div>
+              <div>
+                秒
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-Main">
+        <div className="App-Header">
+          <span className="Header-Text">
+            Animal Crossing: New Horizons Countdown
+          </span>
+        </div>
+        <div className="App-Body">
+          <Countdown
+            date={releaseDate}
+            renderer={renderer}
+          />
+        </div>
+      </div>
     </div>
   );
 }
